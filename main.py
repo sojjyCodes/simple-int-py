afcfc1f46b86924eaede53ce46eea85eebba5d23
sojjyCodes <ebenezeralagbe26@gmail.com>
2020-07-13 00:17:15 +0100

    Got bored so i did this

---
+++ main.py
@@ -0,0 +1,10 @@
+# My python version of the Simple-int
+
+print("Let calculate the simple interest ")
+
+principal = float(input("The principal: "))
+rate = float(input("The rate: "))
+time = float(input("The time: "))
+
+
+print("Simple Interest is",(principal*rate*time))
